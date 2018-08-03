1e9ae3d5e70509504d32119195a61740b641f63b
SarahRuhl <sarah_e_ruhl@yahoo.com>
2018-08-03 09:05:55 -0700

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,28 @@
+
+public class Task {
+	public String id;
+	public String name;
+	public boolean completed;
+	
+	public Task(String id, String name){
+		this.id = id;
+		this.name = name;
+		this.completed = false;
+	}
+	
+	public void completed(){
+		this.completed = true;
+	}
+	
+	public String getTask(){
+		return this.name;
+	}
+	
+	public String getID(){
+		return this.id;
+	}
+	
+	public boolean isCompleted(){
+		return completed;
+	}
+}
